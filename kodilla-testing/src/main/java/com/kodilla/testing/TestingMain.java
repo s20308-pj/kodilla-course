742225114a3df9cc26cba99476133f64f3f7b9ef
Laptop <czapli@o2.pl>
2022-04-10 13:33:24 +0200

    6.2 Added first unit test

--- kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
@@ -1,7 +1,26 @@
 package com.kodilla.testing;
 
+import com.kodilla.testing.calculator.Calculator;
+
 public class TestingMain {
     public static void main(String[] args) {
         System.out.println("Moduł 6. Wprowadzienie do testowania oprogramowania");
+
+        Calculator calculator = new Calculator();
+        //Test add
+    int result = calculator.add(10,20);
+        if (result == 30){
+            System.out.println("test add: PASSED");
+        } else {
+            System.out.println("test add: FAILED");
+        }
+        //Test subtract
+
+        result = calculator.subtract(30,20);
+        if (result == 10){
+            System.out.println("test subtract: PASSED");
+        } else {
+            System.out.println("test subtract: FAILED");
+        }
     }
 }
