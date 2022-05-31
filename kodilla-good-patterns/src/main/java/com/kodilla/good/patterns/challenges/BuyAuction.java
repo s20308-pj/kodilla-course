94afbbe530b512e69afeb3c39a61a0b9382cca3f
Michał Czapliński <czapli@o2.pl>
2022-05-31 13:33:26 +0200

    13.2 Order Service

--- kodilla-good-patterns/src/main/java/com/kodilla/good/patterns/challenges/BuyAuction.java
+++ kodilla-good-patterns/src/main/java/com/kodilla/good/patterns/challenges/BuyAuction.java
@@ -3,7 +3,6 @@ package com.kodilla.good.patterns.challenges;
 public class BuyAuction implements BuyService {
     @Override
     public boolean sell(double sellPrice, double offeredPrice) {
-        System.out.println("seller:" + sellPrice + "\noffered:"+ offeredPrice);
         if (sellPrice <= offeredPrice) {
             return true;
         } else {
