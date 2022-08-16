7d2593494550fcc1fdf67874f103a6154c550f2f
Michał Czapliński <czapli@o2.pl>
2022-08-16 20:31:13 +0200

    24.3 add: OrderWatcher

--- kodilla-patterns2/src/main/java/com/kodilla/patterns2/KodillaApplication.java
+++ kodilla-patterns2/src/main/java/com/kodilla/patterns2/KodillaApplication.java
@@ -2,6 +2,7 @@ package com.kodilla.patterns2;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
 
 @SpringBootApplication
 public class KodillaApplication {
